22c1b64a288870821e46b83acdedea5e0260fe01
Göktürk Göker <gg.gokturk.goker@gmail.com>
2021-10-29 01:47:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCw-2Q5hUQH2YTe6XbRs8WCZ6sKD7tjZYc",
-    authDomain: "coone-330219.firebaseapp.com",
-    projectId: "coone-330219",
-    storageBucket: "coone-330219.appspot.com",
-    messagingSenderId: "226040133408",
-    appId: "1:226040133408:web:d9475e63e72d3bb5ba8945"
+    
   };
 
 const firebaseApp = initializeApp(firebaseConfig);
